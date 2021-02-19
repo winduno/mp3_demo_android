e7157270258307267df8c4a2af591fa31af6f83c
winduno <windsolo294@gmail.com>
2021-02-20 06:15:17 +0700

    android commit 1

---
+++ app/src/main/java/com/example/mp3app/Activity/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.mp3app.Activity;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.example.mp3app.Adapter.MainViewPagerAdapter;
+import com.example.mp3app.Fragment.Fragment_home;
+import com.example.mp3app.Fragment.Fragment_search;
+import com.example.mp3app.R;
+import com.google.android.material.tabs.TabLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    TabLayout tabLayout;
+    ViewPager viewPager;
+    
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_NOTHING);
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        findView();
+        init();
+    }
+
+    private void init() {
+        MainViewPagerAdapter mainViewPagerAdapter = new MainViewPagerAdapter(getSupportFragmentManager());
+        mainViewPagerAdapter.addFragment(new Fragment_home(), "Home");
+        mainViewPagerAdapter.addFragment(new Fragment_search(), "Search");
+        viewPager.setAdapter(mainViewPagerAdapter);
+        tabLayout.setupWithViewPager(viewPager);
+        tabLayout.getTabAt(0).setIcon(R.drawable.icontrangchu);
+        tabLayout.getTabAt(1).setIcon(R.drawable.iconsearch);
+    }
+
+    private void findView() {
+        tabLayout = findViewById(R.id.homeTabLayout);
+        viewPager = findViewById(R.id.homeViewPager);
+    }
+}
